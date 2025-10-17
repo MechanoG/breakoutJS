b76073f4eb6e2acfdce374917806faed22236717
MechanoG <glory_to_the_machine@protonmail.com>
2025-10-17 17:26:13 -0400

    Pruebas para cambiar sprite de bricks

--- game.js
+++ game.js
@@ -7,7 +7,7 @@ const config = {
     physics: {
       default: 'arcade',
       arcade: {
-        debug: false 
+        debug: false,
       }
     },
     parent: 'game',
@@ -27,7 +27,6 @@ function preload(){
   this.load.atlas("ballPadle", "assets/images/paddles_and_balls.png", "assets/images/ballPadle.json");
   this.load.atlas("bricks", "assets/images/bricks.png", "assets/images/bricks.json");
   
-
 }
  
 function create(){
@@ -55,6 +54,8 @@ function create(){
   paddle.setDisplaySize(100, 30);
   paddle.setCollideWorldBounds(true);
 
+  this.add.image(700,300, "bricks", bricksFrames[6]);
+
   //Ball
   ball= this.physics.add.sprite(400,paddle.y-35, "ballPadle", ballpadleFrames[0]).setDisplaySize(30, 30);
   ball.setCollideWorldBounds(true);
@@ -75,21 +76,13 @@ function create(){
     key : "bricks",
     frame: [graybrick,greenbrick,yellowbrick,orangebrick,redbrick,purplebrick],
     frameQuantity : 7,
-    gridAlign :{width: 7, height:6, cellWidth : 100, cellHeight: 30, x: 85, y: 100}
+    gridAlign :{width: 7, height:6, cellWidth : 105, cellHeight: 35, x: 75, y: 100}
    });
 
-  bricks.children.iterate((child) =>{child.setDisplaySize(100, 30)} )
- 
-
-
-
+  bricks.children.iterate((child) =>{
+    child.setDisplaySize(100, 30)
+  } )
   
- 
-  
-  
-           
-
-
   //Collisions
   
   this.physics.add.collider(paddle,ball, hitpaddle);
@@ -100,101 +93,7 @@ function create(){
   cursor = this.input.keyboard.createCursorKeys();
   
 
-  /*this.add.image(400, 300, "sky");
-  
-  platforms = this.physics.add.staticGroup();
-
-  platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-  
-  platforms.create(600, 400, 'ground');
-  platforms.create(50, 250, 'ground');
-  platforms.create(750, 220, 'ground');
-
-  player = this.physics.add.sprite(100, 450, 'dude');
-
-  player.setBounce(0.2);
-  player.setCollideWorldBounds(true);
-  //player.body.setGravityY(300);
-  
-  this.physics.add.collider(player, platforms);
-
-  this.anims.create({
-    key: 'left',
-    frames:this.anims.generateFrameNumbers('dude', {start : 0, end : 3}),
-    framerate: 10,
-    repeat:-1
-  });
-
-  this.anims.create({
-    key: 'turn',
-    frames: [{key: 'dude', frame: 4}],
-    frameRate: 20
-  });
-
-  this.anims.create({
-    key: 'right',
-    frames: this.anims.generateFrameNumbers('dude', {start: 5, end: 8}),
-    frame: 10,
-    repeat: -1
-  });
-
-  cursors = this.input.keyboard.createCursorKeys();
-
-  stars = this.physics.add.group({
-    key: 'star',
-    repeat: 11,
-    setXY: {x : 12, y: 0, stepX: 70 }
-  });
-
-  stars.children.iterate( function(child){
-    child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-  });
-
-  this.physics.add.collider(stars, platforms);
-
-  this.physics.add.overlap(player, stars, collecStar, 
-    null, this);
-
-  function collecStar(player, star){
-    star.disableBody(true, true);
-
-    score +=10;
-    scoreText.setText('Score:' + score);
-
-    if(stars.countAvtive(true) === 0){
-      stars.children.iterate(function (child){
-        child.enableBody(true, child.x, true, true);
-      });
-
-      var x = (player.x < 400) ? Phaser.Math.Between(400,800) : Phaser.Math.Between(0,400);
-
-      var bomb = bombs.create(x, 16, 'bomb');
-      bomb.setBounce(1);
-      bomb.setCollideWorldBounds(true);
-      bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-
-    } 
-  }
-
-  var score = 0;
-  var scoreText;
-
-  scoreText = this.add.text(16, 16, 'Score = 0', {fontSize:
-    '32px', fill: '#000'
-  });
-
-  bombs = this.physics.add.group();
-  this.physics.add.collider(bombs,platforms);
-  this.physics.add.collider(player, bombs, hitbomb, null, this);
-
-  function hitbomb(player, bomb){
-    this.physics.pause();
-    player.setTint(0xff0000);
-    player.anims.play("turn");
-    gameOver = "true";
-  }
-  
-  */
+ 
   
 }
 
@@ -268,24 +167,4 @@ function update(){
     resetBall();
   }
 
-
-
-
-  /* Lógica de actualización
-  if (cursors.left.isDown){
-    player.setVelocityX(-160);
-    player.anims.play('left', true);
-  }
-  else if (cursors.right.isDown){
-    player.setVelocityX(160);
-    player.anims.play('right', true);
-  }
-  else{
-    player.setVelocityX(0);
-    player.anims.play('turn');
-  }
-  
-  if (cursors.up.isDown && player.body.touching.down){
-    player.setVelocityY(-330);
-  }*/
 }
